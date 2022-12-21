d690bdd4489477a069e8c8128d500785283690d0
sangHwan <yajang12@gmail.com>
2022-12-22 00:20:16 +0900

    4th

--- backProject/spring5fs/sp5-chap03/src/main/java/spring/MemberDao.java
+++ backProject/spring5fs/sp5-chap03/src/main/java/spring/MemberDao.java
@@ -2,6 +2,7 @@ package spring;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Collection;
 
 public class MemberDao {
     private static long nextId = 0;
@@ -18,5 +19,9 @@ public class MemberDao {
 
     public void update(Member member){
         map.put(member.getEmail(), member);
+    }
+
+    public Collection<Member> selectAll(){
+        return map.values();
     }
 }
