046424b597b3ad09dee6a7f6360dbaea769cfd17
sangHwan <yajang12@gmail.com>
2023-01-14 00:31:46 +0900

    12th

--- backProject/spring5fs/sp5-chap09/src/main/java/chap09/HelloController.java
+++ backProject/spring5fs/sp5-chap09/src/main/java/chap09/HelloController.java
@@ -1,7 +1,5 @@
 package chap09;
 
-import java.time.LocalDateTime;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +12,7 @@ public class HelloController {
             @RequestParam(value="name", required=false) String name){
         model.addAttribute("greeting", "Hello, " + name);
 
-        System.out.println("hello() 메서드 실행됨: " + LocalDateTime.now());
+        System.out.println("hello() 메서드 실행");
 
         return "hello";
     }
