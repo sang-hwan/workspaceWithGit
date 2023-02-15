a51fe9c456343e02887f830fe1bc918316711084
sang-hwan <75194773+sang-hwan@users.noreply.github.com>
2023-02-15 16:33:58 +0900

    Delete crawling directory

--- crawling/5_user_agent.py
+++
@@ -1,8 +0,0 @@
-import requests
-url = "http://nadocoding.tistory.com"
-# what-is-my-user-agent로 user-agent 확인
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"}
-res = requests.get(url, headers=headers)
-res.raise_for_status()
-with open("nadocoding.html", "w", encoding="utf-8") as f:
-    f.write(res.text)
